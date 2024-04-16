9bff25baef860306926c1052b301900841f4d79f
Joseph LeMoure <joelem316@gmail.com>
2024-04-15 21:03:29 -0400

    user validator change

--- backend/server/models/userValidator.js
+++ backend/server/models/userValidator.js
@@ -2,7 +2,7 @@ const z = require('zod')
 
 const newUserValidation = data => {
   const registerValidationSchema = z.object({
-    username: z.string().min(3, 'Username must be 3 characters or more'),
+    username: z.string().min(3, 'Username must be 3 characters or more').max(18, 'Username must be no more than 18 characters'),
     email: z.string().optional(),
     password: z.string().min(3, 'Password must be 3 or more characters').trim(),
     firstName: z.string().min(1, 'First Name is required'),
@@ -15,7 +15,7 @@ const newUserValidation = data => {
 
 const userLoginValidation = data => {
   const loginValidationSchema = z.object({
-    username: z.string().min(3, 'Username must be 3 characters or more'),
+    username: z.string().min(3, 'Username must be 3 characters or more').max(18, 'Username must be no more than 18 characters'),
     password: z.string().min(3, 'Password must be 3 or more characters').trim(),
   });
   return loginValidationSchema.safeParse(data)
